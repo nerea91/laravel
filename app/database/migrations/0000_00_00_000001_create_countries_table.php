125b66bd2c76fcd8854326d7f843ac3da84ad3a7
Stolz <stolz@gsmlandia.com>
2013-10-02 16:59:32 +0200

    ISO 3166 Countries migration and seeder

---
+++ app/database/migrations/0000_00_00_000001_create_countries_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCountriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('countries', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name', 64)->unique();
+			$table->string('full_name', 128)->nullable();
+			$table->string('iso_3166_2', 2)->unique();
+			$table->string('iso_3166_3', 3)->unique();
+			$table->string('country_code', 3)->unique();
+			$table->string('capital', 64)->nullable();
+			$table->string('citizenship', 64)->nullable();
+			$table->string('currency', 64)->nullable();
+			$table->string('currency_code', 16)->nullable();
+			$table->string('currency_sub_unit', 32)->nullable();
+			$table->string('region_code', 3)->nullable();
+			$table->string('sub_region_code', 3)->nullable();
+			$table->boolean('eea')->unsigned();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('countries');
+	}
+
+}
+

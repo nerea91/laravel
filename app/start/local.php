010943e2b1b45fa8568483d2a9b108b673e8d6be
Stolz <stolz@gsmlandia.com>
2013-10-05 16:23:28 +0200

    Skip SQL strict mode for cli (artisan)

--- app/start/local.php
+++ app/start/local.php
@@ -1,4 +1,6 @@
 <?php
 
 //Enable SQL strict mode in develop environment
-DB::statement('SET SESSION sql_mode="STRICT_ALL_TABLES"');
+if(php_sapi_name() != 'cli')
+	DB::statement('SET SESSION sql_mode="STRICT_ALL_TABLES"');
+

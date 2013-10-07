7ce015ddfc7002c0fbcd28fce41e1bea3e352fd6
Stolz <stolz@gsmlandia.com>
2013-10-07 20:06:13 +0200

    Twitter Bootstrap and Google Analytics

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -38,14 +38,16 @@
 		{{ Assets::js() }}
 		@yield('js')
 
-		{{-- Google Analytics: to-do change UA-XXXXX-X to be your site's ID.  --}}
+		{{-- Google Analytics --}}
+		<?php if(Config::has('site.googleanalytics')) : ?>
 		<script>
 			(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
 			function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
 			e=o.createElement(i);r=o.getElementsByTagName(i)[0];
 			e.src='//www.google-analytics.com/analytics.js';
 			r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-			ga('create','UA-XXXXX-X');ga('send','pageview');
+			ga('create','{{Config::get("site.googleanalytics")}}');ga('send','pageview');
 		</script>
+		<?php endif ?>
 	</body>
 </html>

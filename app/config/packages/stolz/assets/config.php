63afdc9263b5dd8c5c9557a6d127a628e3a5d9b8
Stolz <stolz@gsmlandia.com>
2013-10-12 21:26:03 +0200

    Added Zurb Foundation from CDN

--- app/config/packages/stolz/assets/config.php
+++ app/config/packages/stolz/assets/config.php
@@ -2,76 +2,31 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Library Debug Mode
-	|--------------------------------------------------------------------------
-	|
-	| When debug mode is enabled information about the process of loading
-	| assets will be sent to the log.
-	|
-	*/
+	// 	'autoload' => array(),
 
-// 	'debug' => TRUE,
+	'collections' => array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Local assets directories
-	|--------------------------------------------------------------------------
-	|
-	| Override defaul prefix folder for local assets.
-	| Don't use trailing slash!.
-	|
-	*/
+		//Zurb Foundation 4.3.2 (CDN)
+		'foundation-cdn' => [
+			'//cdn.jsdelivr.net/foundation/4.3.2/css/foundation.min.css',
+			'//cdn.jsdelivr.net/foundation/4.3.2/js/foundation.min.js'
+		],
 
-	//'css_dir' => '/css',
-	//'js_dir' => '/js',
+		//Twitter Bootstrap 3 (CDN)
+		'bootstrap-cdn' => [
+			'//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css',
+			'//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css',
+			'//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js'
+		],
 
-	/*
-	|--------------------------------------------------------------------------
-	| Assets collections
-	|--------------------------------------------------------------------------
-	|
-	| Collections allow you to have named groups of assets (CSS or JavaScript files).
-	|
-	| If an asset has been loaded already it won't be added again. Collections may be
-	| nested but please be careful to avoid recursive loops.
-	|
-	| To avoid conflicts with the autodetection of asset types make sure your
-	| collections names dont end with ".js" or ".css".
-	|
-	|
-	| Example:
-	|	'collections' => array(
-	|		'uno'	=> 'uno.css',
-	|		'dos'	=> ['dos.css', 'dos.js'],
-	|		'external'	=> ['http://example.com/external.css', 'https://secure.example.com/https.css', '//example.com/protocol/agnostic.js'],
-	|		'mix'	=> ['internal.css', 'http://example.com/external.js'],
-	|		'nested' => ['uno', 'dos'],
-	|		'duplicated' => ['nested', 'uno.css','dos.css', 'tres.js'],
-	|		'unknown' => 'xxxxxxx',
-	|	),
-	*/
+		/* Twitter Bootstrap 3 (local). To get Bootstrap:
+		composer require twitter/bootstrap:3.0.*; artisan asset:publish twitter/bootstrap --path=vendor/twitter/bootstrap/dist/)
+		*/
+		'bootstrap' => [
+			'twitter/bootstrap:bootstrap.min.css',
+			'twitter/bootstrap:bootstrap-theme.min.css',
+			'twitter/bootstrap:bootstrap.min.js'
+		],
+	),
 
-		'collections' => array(
-
-			//Twitter Bootstrap 3 (CDN)
-			'bootstrap-cdn' => ['//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js'],
-
-			/* Twitter Bootstrap 3 (local). To get Bootstrap:
-			$ composer require twitter/bootstrap:3.0.*
-			$ artisan asset:publish twitter/bootstrap --path=vendor/twitter/bootstrap/dist/)*/
-			'bootstrap' => [url('packages/twitter/bootstrap/css/bootstrap.min.css'), url('packages/twitter/bootstrap/css/bootstrap-theme.min.css'), url('packages/twitter/bootstrap/js/bootstrap.min.js')],
-		),
-
-	/*
-	|--------------------------------------------------------------------------
-	| Preload assets
-	|--------------------------------------------------------------------------
-	|
-	| Here you may set which assets (CSS files, JavaScript files or collections)
-	| should be loaded by default.
-	|
-	*/
-// 	'autoload' => array(),
 );

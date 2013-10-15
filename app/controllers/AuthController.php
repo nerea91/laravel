f4ddb9ed868b3a2820dab4b4c7aacc0546003009
Stolz <stolz@gsmlandia.com>
2013-10-15 21:21:59 +0200

    Fixed 'remember me'

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends BaseController {
 
 		if($validator->passes())
 		{
-			if(Auth::attempt(array_except($input, 'remember'), Input::has('remember'))) //to-do se acuerda del login siempre auqnue no se marque el checkbox
+			if(Auth::attempt(array_except($input, 'remember'), Input::has('remember')))
 				return Redirect::intended('/');
 
 			Session::flash('message', _('Wrong credentials'));

48b47bf5c4d22a7a0bd9fec34ce3503905fc675a
Stolz <stolz@gsmlandia.com>
2013-10-16 00:13:01 +0200

    Added jQuery assets collection

--- app/config/packages/stolz/assets/config.php
+++ app/config/packages/stolz/assets/config.php
@@ -2,27 +2,41 @@
 
 return array(
 
-	// 	'autoload' => array(),
+	//'autoload' => array(),
 
 	'collections' => array(
 
-		//Zurb Foundation 4.3.2 (CDN)
-		'foundation-cdn' => [
-			'//cdn.jsdelivr.net/foundation/4.3.2/css/foundation.min.css',
-			'//cdn.jsdelivr.net/foundation/4.3.2/js/foundation.min.js'
+		//jQuery (CDN)
+		'jquery-cdn' => ['//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'],
+
+		//jQuery UI (CDN)
+		'jquery-ui-cdn' => [
+			'jquery-cdn',
+			'//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js',
+			//Uncomment to load i18n for all languages other than English '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/i18n/jquery-ui-i18n.min.js',
+			//Uncomment to load i18n for only one language (i.e: Spanish) '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/i18n/jquery.ui.datepicker-es.min.js',
+			'//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.min.css',
 		],
 
-		//Twitter Bootstrap 3 (CDN)
+		//Zurb Foundation (CDN)
+		'foundation-cdn' => [
+			'jquery-cdn',
+			'//cdn.jsdelivr.net/foundation/4.3.2/css/foundation.min.css',
+			'//cdn.jsdelivr.net/foundation/4.3.2/js/foundation.min.js',
+			'app.js'
+		],
+
+		//Twitter Bootstrap (CDN)
 		'bootstrap-cdn' => [
+			'jquery-cdn',
 			'//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css',
 			'//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css',
 			'//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js'
 		],
 
-		/* Twitter Bootstrap 3 (local). To get Bootstrap:
-		composer require twitter/bootstrap:3.0.*; artisan asset:publish twitter/bootstrap --path=vendor/twitter/bootstrap/dist/)
-		*/
+		// Twitter Bootstrap. To install it run: composer require twitter/bootstrap:3.0.*; artisan asset:publish twitter/bootstrap --path=vendor/twitter/bootstrap/dist/
 		'bootstrap' => [
+			'jquery-cdn',
 			'twitter/bootstrap:bootstrap.min.css',
 			'twitter/bootstrap:bootstrap-theme.min.css',
 			'twitter/bootstrap:bootstrap.min.js'

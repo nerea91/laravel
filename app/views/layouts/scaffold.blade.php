138a2dfa29e096af7628d91d986fcef5ae80a98f
Stolz <stolz@gsmlandia.com>
2013-10-23 16:04:05 +0200

    m

--- app/views/layouts/scaffold.blade.php
+++ app/views/layouts/scaffold.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.base')
 
-@section('css')
-<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
-<style>
-	table form { margin-bottom: 0; }
-	form ul { margin-left: 0; list-style: none; }
-	.error { color: red; font-style: italic; }
-	body { padding-top: 20px; }
-</style>
-@stop
-
 @section('body')
-<div class="container">
+
+<div class="row">
+
+	<h3>{{ $title }}</h3>
+
 	@if (Session::has('message'))
-	<div class="flash alert">
-		<p>{{ Session::get('message') }}</p>
+	<div class="alert-box alert radius">
+		{{ Session::get('message') }}
 	</div>
 	@endif
 
 	@yield('main')
+
 </div>
 @stop

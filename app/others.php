13460a1eb475c2f28a129164d1ecc5cfdb4bbb96
Stolz <stolz@gsmlandia.com>
2013-10-26 00:39:49 +0200

    typo

--- app/others.php
+++ app/others.php
@@ -1,15 +1,15 @@
 <?php
 
-+/*
-+|--------------------------------------------------------------------------
-+| Application Routes
-+|--------------------------------------------------------------------------
-+|
-+| Here is where you can register all of the routes for an application.
-+| It's a breeze. Simply tell Laravel the URIs it should respond to
-+| and give it the Closure to execute when that URI is requested.
-+|
-+*/
+/*
+|--------------------------------------------------------------------------
+| Application Extensions
+|--------------------------------------------------------------------------
+|
+| Here is where you can add all the snippets that your application needs
+| other than routes or filters (i.e: view composers, auth drivers, custom
+| validation rules, etc ...).
+|
+*/
 
 //View composers
 View::composer('layouts.base', 'BaseLayoutComposer');

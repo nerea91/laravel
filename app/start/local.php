0df5c7051424f1e97e5fc51cda7f8019fd9fb99d
Stolz <stolz@gsmlandia.com>
2013-10-26 14:54:35 +0200

    fix for testing enviorment

--- app/start/local.php
+++ app/start/local.php
@@ -1,6 +1,6 @@
 <?php
 
 //Enable SQL strict mode in develop environment
-if(php_sapi_name() != 'cli')
+if ( ! $app->runningInConsole())
 	DB::statement('SET SESSION sql_mode="STRICT_ALL_TABLES"');
 

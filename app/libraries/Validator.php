09ba2fc315f77044f85abaaf14a40f47f1f490a6
Stolz <stolz@gsmlandia.com>
2013-11-12 15:15:04 +0100

    New validation rules

--- app/libraries/Validator.php
+++ app/libraries/Validator.php
@@ -1,6 +1,23 @@
 <?php namespace Stolz\Validation;
 
-class Validator extends \Illuminate\Validation\Validator{
+class Validator extends \Illuminate\Validation\Validator {
+
+
+	/**
+	 * Create a new Validator instance.
+	 *
+	 * @param  \Symfony\Component\Translation\TranslatorInterface  $translator
+	 * @param  array  $data
+	 * @param  array  $rules
+	 * @param  array  $messages
+	 * @return void
+	 */
+	public function __construct(TranslatorInterface $translator, $data, $rules, $messages = array())
+	{
+		parent::__construct($translator, $data, $rules, $messages);
+
+		$this->numericRules[] = 'Positive';
+	}
 
 	/**
 	 * Validate that an attribute contains only alphabetic characters and spaces.
@@ -36,5 +53,17 @@ class Validator extends \Illuminate\Validation\Validator{
 	protected function validateAlphaDashSpace($attribute, $value)
 	{
 		return preg_match('/^[\pL\pN_-\s]+$/u', $value);
+	}
+
+	/**
+	 * Validate that an attribute is numeric and greather than 0.
+	 *
+	 * @param  string  $attribute
+	 * @param  mixed   $value
+	 * @return bool
+	 */
+	protected function validatePositive($attribute, $value)
+	{
+		return is_numeric($value) and $value > 0;
 	}
 }

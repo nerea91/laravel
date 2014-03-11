5fb91a4020a801b8d0f278e214febc2da0e06a27
Stolz <stolz@gsmlandia.com>
2014-03-11 16:01:36 +0100

    Typo

--- app/controllers/BaseResourceController.php
+++ app/controllers/BaseResourceController.php
@@ -58,7 +58,7 @@ class BaseResourceController extends \BaseController {
 	protected function index()
 	{
 		// Paginate resource resutls
-		$results = $this->resource->OrderByUrl()->with(func_get_args())->paginate();
+		$results = $this->resource->orderByUrl()->with(func_get_args())->paginate();
 
 		// If results found add asset to make tables responsive
 		//$results->getTotal() and Assets::add('responsive-tables');

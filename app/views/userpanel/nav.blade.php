cba90228e3d09639ef5876ef7ff9315eea029e72
Stolz <stolz@gsmlandia.com>
2014-06-09 19:15:11 +0200

    Fix for problem after latest Laravel update

--- app/views/userpanel/nav.blade.php
+++ app/views/userpanel/nav.blade.php
@@ -1,6 +1,6 @@
 <hr/>
 <dl class="sub-nav">
 	<dt>{{ _('Sections') }}</dt>
-	<dd class="@if($active == 'options')active@endif"><a href="{{ route('user.options') }}">{{ _('Options') }}</a></dd>
-	<dd class="@if($active == 'password')active@endif"><a href="{{ route('user.password') }}">{{ _('Password') }}</a></dd>
+	<dd class="@if($active == 'options')active @endif"><a href="{{ route('user.options') }}">{{ _('Options') }}</a></dd>
+	<dd class="@if($active == 'password')active @endif"><a href="{{ route('user.password') }}">{{ _('Password') }}</a></dd>
 </dl>

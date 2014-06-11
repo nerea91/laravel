05b83a18f4088a189e8a47f23364427bac39fd49
Stolz <stolz@gsmlandia.com>
2014-06-11 19:35:37 +0200

    show one more column

--- app/views/admin/users/index.blade.php
+++ app/views/admin/users/index.blade.php
@@ -1,6 +1,7 @@
 <thead>
 	<tr>
-		<th>{{ $links->username }}</th>
+		<th class="actions text-center">{{ $links->username }}</th>
+		<th>{{ $links->name }}</th>
 		<th>{{ $links->profile_id }}</th>
 		<th>{{ $links->country_id }}</th>
 		<th class="actions text-center">{{ _('Actions') }}</th>
@@ -10,7 +11,14 @@
 <tbody>
 	@foreach ($results as $resource)
 	<tr>
-		<td><span data-tooltip class="has-tip" title="{{ $resource->name }}">{{ $resource->username }}</span></td>
+		<td class="actions text-center">
+			@if (strlen($resource->description))
+			<span data-tooltip class="has-tip" title="{{ $resource->description }}">{{ $resource->username }}</span>
+			@else
+			{{ $resource->username }}
+			@endif
+		</td>
+		<td>{{ $resource->name }}</td>
 		<td>{{ $resource->profile->name }}</td>
 		<td>{{ (isset($resource->country->name)) ? $resource->country->name : null }}</td>
 		@include('resource.actions')

9980e7b9545fb1eb5e0e0043dcb775d7ed3f559a
Stolz <stolz@gsmlandia.com>
2014-08-19 17:57:31 +0200

    Consistent error messages

--- app/views/admin/currencies/fields.blade.php
+++ app/views/admin/currencies/fields.blade.php
@@ -3,8 +3,8 @@
 	{{ Form::label($field, $label) }}
 	@if ($field === 'symbol_position')
 
-		@if($errors->has($field))<small class="error">{{ $errors->first($field) }}</small>@endif
 		{{ Form::radios($field, [_('Left'), _('Right')]) }}
+		@if($errors->has($field))<small class="error">{{ $errors->first($field) }}</small>@endif
 
 	@else
 		{{ Form::text($field) }}

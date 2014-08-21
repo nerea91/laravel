5cb01851ec47fe54a09c1197165cedaff5477ba5
Stolz <stolz@gsmlandia.com>
2014-08-21 10:24:24 +0200

    Scripts directory renamed to bin

--- app/routes.php
+++ app/routes.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
 // Home page
 Route::get('/', array('as' => 'home', 'uses' => 'HomeController@showMainPage'));
 

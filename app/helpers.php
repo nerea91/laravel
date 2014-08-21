969b26c7e3ece603e7e357ed8854299485eee4a7
Stolz <stolz@gsmlandia.com>
2014-08-21 17:44:10 +0200

    Trim meaningless decimal part

--- app/helpers.php
+++ app/helpers.php
@@ -84,6 +84,8 @@ if( ! function_exists('format_number'))
 	/**
 	 * Format a number with optional symbol.
 	 *
+	 * NOTE If decimal part is 0 it will be trimed.
+	 *
 	 * @param  float  $number The number being formatted.
 	 * @param  string $decimalSep Separator for the decimal point.
 	 * @param  string $thousandsSep Separator for thousands.
@@ -93,8 +95,20 @@ if( ! function_exists('format_number'))
 	 */
 	function format_number($number, $precision = 2, $decimalSep = '.', $thousandsSep = null, $symbol = null, $symbolToTheRight = true)
 	{
-		$formatted = number_format(sprintf('%F', $number), intval($precision), $decimalSep, $thousandsSep);
+		// Format number
+		$number = floatval(sprintf('%F', $number));
+		$precision = intval($precision);
+		$formatted = number_format($number, $precision, $decimalSep, $thousandsSep);
 
+		// Trim meaningless ending zeros
+		if($precision and strlen($decimalSep))
+		{
+			$safeDecimalSep = preg_quote($decimalSep, '/');
+			$meaningless = str_repeat(0, $precision);
+			$formatted = preg_replace("/{$safeDecimalSep}{$meaningless}$/", null, $formatted, 1);
+		}
+
+		// Add symbol
 		if( ! strlen($symbol))
 			return $formatted;
 

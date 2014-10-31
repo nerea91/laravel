e5744a576b66fc17377aa0e2ce9222839ba012bb
Stolz <Stolz@users.noreply.github.com>
2014-10-31 16:35:44 +0100

    QuickCommit

--- app/Console/SyncDatabaseMetaTablesCommand.php
+++ app/Console/SyncDatabaseMetaTablesCommand.php
@@ -14,6 +14,7 @@ class SyncDatabaseMetaTablesCommand extends Command
 	 */
 	protected $tables = [
 		// Table          => Primary key
+		'options'         => 'name',
 		'permissiontypes' => 'id',
 		'permissions'     => 'id',
 	];

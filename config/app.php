c0248f17ad498f71d1dc7199981f0c84c50162bb
Stolz <Stolz@users.noreply.github.com>
2015-06-12 13:54:41 +0200

    CodeSniffer

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
 	'key' => env('APP_KEY', 'SomeRandomString'), //TODO
 
-    'cipher' => 'AES-256-CBC',
+	'cipher' => 'AES-256-CBC',
 
 	/*
 	|--------------------------------------------------------------------------

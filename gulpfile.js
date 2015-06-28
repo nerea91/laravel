80ecd9e1735819a88282b077a01cc61fd146d9da
Stolz <Stolz@users.noreply.github.com>
2015-06-28 15:29:50 +0200

    Remove unused Zurb Foundation components

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,45 @@
+// Bower components path
+var bower = 'resources/assets/bower_components/';
+
+// Destination dir for compiled componentes
+var destination = 'public/packages/zurb/foundation/';
+
+// Zurb Foundation vendor dependencies
+var vendor = ['vendor/modernizr.js', 'vendor/jquery.js'];
+
+// Zurb Foundation components
+var components = [
+	// All components
+	// 'foundation.min.js'
+
+	// Only some components
+	'foundation/foundation.js',
+	//'foundation/foundation.abide.js',
+	'foundation/foundation.accordion.js',
+	'foundation/foundation.alert.js',
+	//'foundation/foundation.clearing.js',
+	'foundation/foundation.dropdown.js',
+	'foundation/foundation.equalizer.js',
+	//'foundation/foundation.interchange.js',
+	//'foundation/foundation.joyride.js',
+	'foundation/foundation.magellan.js',
+	'foundation/foundation.offcanvas.js',
+	//'foundation/foundation.orbit.js',
+	'foundation/foundation.reveal.js',
+	//'foundation/foundation.slider.js',
+	'foundation/foundation.tab.js',
+	'foundation/foundation.tooltip.js',
+	'foundation/foundation.topbar.js'
+];
+
 var elixir = require('laravel-elixir');
-
 elixir.config.sourcemaps = false;
-
 elixir(function(mix) {
 
 	// Compile CSS
-	mix.sass(
-		'app.scss', // Source files
-		'public/packages/zurb/foundation/css', // Destination folder
-		{includePaths: ['resources/assets/bower_components/foundation/scss']}
-	);
+	mix.sass('app.scss', destination + 'css', {includePaths: [bower + 'foundation/scss']});
 
 	// Compile JavaScript
-	mix.scripts(
-		['vendor/modernizr.js', 'vendor/jquery.js', 'foundation.min.js'], // Source files
-		'public/packages/zurb/foundation/js/app.js', // Destination file
-		'resources/assets/bower_components/foundation/js/' // Source files base directory
-	);
+	mix.scripts(vendor.concat(components), destination + 'js/app.js', bower + 'foundation/js' );
 
 });

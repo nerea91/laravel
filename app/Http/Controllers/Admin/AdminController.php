660d2bbab930e07d5bf10a5cd421a0d72a4f094f
Stolz <Stolz@users.noreply.github.com>
2015-07-14 15:28:47 +0200

    Added search filters

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -1,30 +1,51 @@
 <?php namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Auth;
 use Cache;
-use Illuminate\Support\Collection;
-use Input;
-use Route;
-use Session;
-use View;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
 
 class AdminController extends Controller
 {
+	use ValidatesRequests;
+
 	protected $layout = 'layouts.admin';
+
+	protected $searchableModels = [
+		10  => \App\Country::class,
+		20  => \App\Language::class,
+		40  => \App\Profile::class,
+		60  => \App\User::class,
+		80  => \App\AuthProvider::class,
+		100 => \App\Account::class,
+		120 => \App\Currency::class,
+		140 => \App\Document::class,
+	];
 
 	/**
 	 * Show admin control panel main page
 	 *
 	 * @return Response
 	 */
-	public function showAdminPage()
+	public function showAdminPage(Request $request)
 	{
+		$user = $request->user();
+		$models = $this->getSearchableModels($user);
+
+		// Sort models by plural name
+		foreach($models as $permission => $model)
+			$models[$permission] = $model->plural();
+		asort($models);
+
 		// Get results from previous search
-		$searchResults = Cache::get('adminSearchResults' . Auth::user()->getKey(), false);
+		$searchResults = Cache::get('adminSearchResults' . $user->getKey(), false);
 
 		// Add data to the view
-		$view = view('admin.search')->withSearchResults($searchResults);
+		$view = view('admin.search', [
+			'searchResults' => $searchResults,
+			'models' => $models,
+		]);
 
 		// Add data to the layout
 		$this->layout->withTitle(_('Admin panel'))->withSubtitle(_('Search'));
@@ -44,77 +65,94 @@ class AdminController extends Controller
 	 *
 	 * @return Response
 	 */
-	public function search()
+	public function search(Request $request, Route $route)
 	{
-		$query = Input::get('search');
-		$user = Auth::user();
+		$user = $request->user();
+		$searchableModels = $this->getSearchableModels($user);
+
+		// Validate form
+		$this->validate($request, [
+			'query' => 'required|min:1',
+			'sections' => 'required|array|in:' . implode(',', array_keys($searchableModels))
+		]);
+
+		// Remove unselected models
+		$selectedModels = $request->input('sections', []);
+		foreach($searchableModels as $permission => $model)
+			if( ! in_array($permission, $selectedModels))
+				unset($searchableModels[$permission]);
+
+		// Initializate results
+		$query = $request->input('query');
+		$session = $request->session();
+		$currentRoute = $route->getName();
 		$cacheId = 'adminSearchResults' . $user->getKey();
+		$results = collect();
+		$totalResults = 0;
 
-		if(strlen($query) > 0)
+		// Perform search for all searchable models
+		foreach($searchableModels as $model)
 		{
-			$results = new Collection;
-			$totalResults = 0;
-			$currentRoute = Route::current()->getName();
-			$searchableModels = [
-				10  => 'Country',
-				20  => 'Language',
-				40  => 'Profile',
-				60  => 'User',
-				80  => 'AuthProvider',
-				100 => 'Account',
-				120 => 'Currency',
-				140 => 'Document',
-			];
+			$collection = $model->search($query);
+			if( ! $count = $collection->count())
+				continue;
 
-			// Perform search for all searchable models
-			foreach($searchableModels as $permission => $modelName)
-			{
-				if($user->hasPermission($permission))
-				{
-					$namespacedModel = "\App\\$modelName";
-					$model = new $namespacedModel;
-					$collection = $model->search($query);
-					if( ! $count = $collection->count())
-						continue;
+			$totalResults += $count;
+			$modelName = class_basename($model);
 
-					$totalResults += $count;
-					$result = new \stdClass();
-					$result->label = $model->plural();
-					$result->route = replace_last_segment($currentRoute, strtolower(str_plural($modelName)) . '.show');
-					$result->collection = $collection;
-					$results[$modelName] = $result;
-				}
-			}
-
-			// Results found
-			if($results)
-			{
-				// Sort results showing first the models with smaller collections
-				$results = $results->sort(function ($result1, $result2) {
-					$count1 = $result1->collection->count();
-					$count2 = $result2->collection->count();
-
-					// Is same count order alphabetically
-					if($count1 === $count2)
-						return strcasecmp($result1->label, $result2->label);
-
-					return ($count1 < $count2) ? -1 : 1;
-				});
-
-				// Store results in the cache for 5 minutes
-				Cache::put($cacheId, $results, 5);
-				Session::flash('success', sprintf(_('%d results found'), $totalResults));
-			}
-			// No results found
-			else
-			{
-				Cache::forget($cacheId);
-				Session::flash('error', _('No results found'));
-			}
+			// Build results object
+			$result = new \stdClass();
+			$result->label = $model->plural();
+			$result->route = replace_last_segment($currentRoute, strtolower(str_plural($modelName)) . '.show');
+			$result->collection = $collection;
+			$results[$modelName] = $result;
 		}
+
+		// Results found
+		if($results->count())
+		{
+			// Sort results showing first the models with smaller collections
+			$results = $results->sort(function ($result1, $result2) {
+				$count1 = $result1->collection->count();
+				$count2 = $result2->collection->count();
+
+				// Is same count order alphabetically
+				if($count1 === $count2)
+					return strcasecmp($result1->label, $result2->label);
+
+				return ($count1 < $count2) ? -1 : 1;
+			});
+
+			// Store results in the cache for 5 minutes
+			Cache::put($cacheId, $results, 5);
+			$session->flash('success', sprintf(_('%d results found'), $totalResults));
+		}
+		// No results found
 		else
+		{
 			Cache::forget($cacheId);
+			$session->flash('error', _('No results found'));
+		}
 
 		return redirect()->route('admin')->withInput();
+	}
+
+	/**
+	 * Get the models that provided $user is allowed to seach.
+	 *
+	 * @param   App\User $user
+	 *
+	 * @return array
+	 */
+	protected function getSearchableModels(\App\User $user)
+	{
+		$models = [];
+
+		// Filter
+		foreach($this->searchableModels as $permission => $modelName)
+			if($user->hasPermission($permission))
+				$models[$permission] = new $modelName;
+
+		return $models;
 	}
 }
--- resources/views/admin/search.blade.php
+++ resources/views/admin/search.blade.php
@@ -1,47 +1,52 @@
 @section('main')
-<div class="row">
-	<div class="small-11 small-centered large-6 large-centered columns">
+@if ($models)
+	<div class="row">
+		<div class="small-11 small-centered large-6 large-centered columns">
 
-		{!! Form::open(['route' => 'admin.search']) !!}
+			{!! Form::open(['route' => 'admin.search']) !!}
 
-		<div class="row collapse">
-			<div class="small-7 columns">
-				{!! Form::text('search', null, ['autofocus', 'autocomplete' => 'off']) !!}
-			</div>
-			<div class="small-5 columns">
-				{!! Form::submit(_('Search'), ['class' => 'button postfix']) !!}
-			</div>
+				{!! Form::checkboxes($f = 'sections', $models, array_keys($models), ['legend' => _('Search in')]) !!}
+				@if($errors->has($f))<small class="error">{{ $errors->first($f) }}</small>@endif
+
+				<div class="row collapse">
+					<div class="small-7 columns">
+						{!! Form::text('query', null, ['autofocus', 'autocomplete' => 'off']) !!}
+					</div>
+					<div class="small-5 columns">
+						{!! Form::submit(_('Search'), ['class' => 'button postfix']) !!}
+					</div>
+				</div>
+
+			{!! Form::close() !!}
 		</div>
 
-		{!! Form::close() !!}
-	</div>
+		{{-- Show search results --}}
+		@if ($searchResults)
 
-	{{-- Show search results --}}
-	@if ($search_results)
+			{{-- If more than 1 kind of result show nav bar --}}
+			@if (count($searchResults) > 1)
+			<div data-magellan-expedition="fixed" data-options="destination_threshold:110">
+				<dl class="sub-nav">
+					@foreach ($searchResults as $model => $results)
+					<dd data-magellan-arrival="{{ $model }}"><a href="/admin#{{ $model }}">{{ $results->label }} ({{ $results->collection->count() }})</a></dd>
+					@endforeach
+				</dl>
+			</div>
+			@endif
 
-		{{-- If more than 1 kind of result show nav bar --}}
-		@if (count($search_results) > 1)
-		<div data-magellan-expedition="fixed" data-options="destination_threshold:110">
-			<dl class="sub-nav">
-				@foreach ($search_results as $model => $results)
-				<dd data-magellan-arrival="{{ $model }}"><a href="#{{ $model }}">{{ $results->label }}</a></dd>
-				@endforeach
-			</dl>
-		</div>
+			@foreach ($searchResults as $model => $results)
+			<a name="{{ $model }}"></a>
+			<div class="panel" data-magellan-destination="{{ $model }}">
+				<ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4">
+					<li><h4>{{ $results->label }}</h4></li>
+					@foreach ($results->collection as $model)
+					<li>{!! link_to_route($results->route, $model, array($model->getKey())) !!}</li>
+					@endforeach
+				</ul>
+			</div>
+			@endforeach
 		@endif
 
-		@foreach ($search_results as $model => $results)
-		<a name="{{ $model }}"></a>
-		<div class="panel" data-magellan-destination="{{ $model }}">
-			<ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4">
-				<li><h4>{{ $results->label }}</h4></li>
-				@foreach ($results->collection as $model)
-				<li>{!! link_to_route($results->route, $model, array($model->getKey())) !!}</li>
-				@endforeach
-			</ul>
-		</div>
-		@endforeach
-	@endif
-
-</div>
+	</div>
+@endif
 @stop

9c2048bd6e8255c517cb3008ce282bdf899d75b2
Stolz <Stolz@users.noreply.github.com>
2015-07-17 11:49:47 +0200

    Spinner decoupled from forms

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,17 @@
 if(typeof $ != 'undefined') {
 
-	var $doc = $(document);
+	var $doc = $(document), $window = $(window);
 
-	// This event is specific to jQuery. Occurs first, after the HTML-Document is loaded and DOM is ready.
 	$doc.ready(function() {
 
 		// Zurb Foundation
 		if($.fn.foundation)
-		{
 			$doc.foundation();
 
-			$('form').not('.nospinner').submit(function() {
-				$(':input[type=submit]', $(this)).prop("disabled", true);
-				$('body').append('<span class="spinner"></span>');
-				return true;
-			});
-		}
 	});
 
 	//Checkboxes togglers
-	$('.checkbox_togglers a').click(function(e){
+	$('.checkbox_togglers a').click(function(e) {
 		e.preventDefault();
 		var $scope = ($(this).attr('rel') !== undefined) ? $(this).attr('rel') : $(this).parent().parent();
 		var $checkboxes = $('input:checkbox', $scope);
@@ -39,11 +31,15 @@ if(typeof $ != 'undefined') {
 		}
 	});
 
-	// This event is a standard event in the DOM. Occurs later, after all content (e.g. images, frames,...) are fully loaded.
-	/*$(window).load(function() {
-		...
-	});*/
+	// Show spinner when leaving the page
+	$window.on('beforeunload', function() {
+		$('body').append('<span class="spinner"></span>');
+	});
+
+	// Disable form button after submit
+	$('form').not('dontDisable').submit(function() {
+		$(':input[type=submit]', $(this)).prop('disabled', true);
+		return true;
+	});
+
 }
-
-
-
--- resources/views/layouts/report.blade.php
+++ resources/views/layouts/report.blade.php
@@ -23,7 +23,7 @@
 
 			{{-- FORM --}}
 			<aside class="left-off-canvas-menu">
-				{!! Form::open(['route' => $action, 'class' => 'nospinner']) !!}
+				{!! Form::open(['route' => $action, 'class' => 'dontDisable']) !!}
 					@yield('form')
 					{!! Form::submit(_('Submit'), ['class' => 'button expand']) !!}
 				{!! Form::close() !!}

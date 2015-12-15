c1b364a8189853c577fcd03d0570828049cb77d0
Nerea <nerea@linux-cija.site>
2015-12-15 17:50:56 +0100

    create

--- resources/views/resource/create.blade.php
+++ resources/views/resource/create.blade.php
@@ -1,24 +1,28 @@
 @section('main')
-	<div class="small-11 small-centered large-6 large-centered columns">
-		{!! Form::model($resource, array('route' => "$prefix.store")) !!}
+	<div class="small-12">
+		<div class="small-11 small-centered large-6 large-centered columns">
+			{!! Form::model($resource, array('route' => "$prefix.store")) !!}
 
-		@include("$prefix.fields")
+			@include("$prefix.fields")
 
-		<div class="row">
-			<div class="large-6 columns">
-				{!! Form::submit(_('Add'), array('class' => 'button expand')) !!}
+			<div class="row">
+				<div class="small-12">
+					<div class="large-6 columns">
+						{!! Form::submit(_('Add'), array('class' => 'button expand')) !!}
+					</div>
+
+					<div class="large-6 columns">
+						{{-- If the referer page has a 'page' parameter redirect to there --}}
+						@if (false !== strpos(URL::previous(), '?page=') )
+							<a href="{{ URL::previous() }}" class="secondary button expanded">{{ _('Return') }}</a>
+						@else
+							{!! link_to_route("$prefix.index", _('Return'), [], array('class' => 'secondary button expanded')) !!}
+						@endif
+					</div>
+				</div>
 			</div>
 
-			<div class="large-6 columns">
-				{{-- If the referer page has a 'page' parameter redirect to there --}}
-				@if (false !== strpos(URL::previous(), '?page=') )
-					<a href="{{ URL::previous() }}" class="secondary button expand">{{ _('Return') }}</a>
-				@else
-					{!! link_to_route("$prefix.index", _('Return'), [], array('class' => 'secondary button expand')) !!}
-				@endif
-			</div>
+			{!! Form::close() !!}
 		</div>
-
-		{!! Form::close() !!}
 	</div>
 @stop
